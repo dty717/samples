fcceeaba56ae61a64bdd6753a99d6d03a7f3e3a4
1875167603 <1875167603@qq.com>
2018-11-28 17:04:25 +0800

    build fail

--- src/content/getusermedia/record/README.md
+++ src/content/getusermedia/record/README.md
@@ -4,17 +4,26 @@ start camera-->start recording-->play-->download
 
 the funcitons involved in
 ```
-    start camera  (main.js/go_136)    & (main.js/go_126)& (main.js/go_118)
+    start camera  (main.js/go_141)    & (main.js/go_131)& (main.js/go_123)
              addEventListener('click')& initCamera&        handleSuccess
     
-    start recording  (main.js/go_25)      &       (main.js/go_74)
+    start recording  (main.js/go_25)      &       (main.js/go_97)  &    (main.js/go_72)
+                addEventListener('click') &        startRecording  &   handleDataAvailable
+    
+    end   recording  (main.js/go_25)      &       (main.js/go_119)
                 addEventListener('click') &        startRecording
-                
+    
+       
     play  (main.js/go_37) 
       addEventListener('click')
     
     download   (main.js/go_47)
         addEventListener('click')
-    
+        
+    test  (main.js/go_47)
+        
+        
 ```
+
+so when start camera, start recording for 1 second, then download
 
--- src/content/getusermedia/record/js/main.js
+++ src/content/getusermedia/record/js/main.js
@@ -35,14 +35,17 @@ recordButton.addEventListener('click', () => {
 
 const playButton = document.querySelector('button#play');
 playButton.addEventListener('click', () => {
-  const superBuffer = new Blob(recordedBlobs, {type: 'video/webm'});
+  play(recordedBlobs)
+});
+function play(blob){
+  const superBuffer = new Blob(blob, {type: 'video/webm'});
   recordedVideo.src = null;
   recordedVideo.srcObject = null;
   recordedVideo.src = window.URL.createObjectURL(superBuffer);
   recordedVideo.controls = true;
   recordedVideo.play();
-});
-
+}
+var idVideo=0;
 const downloadButton = document.querySelector('button#download');
 downloadButton.addEventListener('click', () => {
   const blob = new Blob(recordedBlobs, {type: 'video/webm'});
@@ -52,38 +55,69 @@ downloadButton.addEventListener('click', () => {
   a.href = url;
   a.download = 'test.webm';
   document.body.appendChild(a);
-  a.click();
+  //a.click();
   setTimeout(() => {
     document.body.removeChild(a);
-    window.URL.revokeObjectURL(url);
+    ws.send('filename:'+(idVideo++)+"test.webm");
+    ws.send(blob);
+    ws.send('end');
+    console.log("upload ok");
   }, 100);
 });
 
 function handleSourceOpen(event) {
-  console.log('MediaSource opened');
   sourceBuffer = mediaSource.addSourceBuffer('video/webm; codecs="vp8"');
-  console.log('Source buffer: ', sourceBuffer);
 }
+var containerBlobs=[];
+var one=false;
 
 function handleDataAvailable(event) {
   if (event.data && event.data.size > 0) {
-    recordedBlobs.push(event.data);
+    if(recordedBlobs.length<170){
+        recordedBlobs.push(event.data);
+    }else{
+        //no safe I think, but it work in chrome.
+        // containerBlobs=recordedBlobs;
+        // recordedBlobs=null;
+        // recordedBlobs=[];
+        if(ws==undefined||(ws.readyState!=1))    
+            return;
+        if(one){
+            return
+        }
+        recordedBlobs.push(event.data);
+        if(recordedBlobs.length<1600){
+             return;
+        }
+        one=true
+        const blob = new Blob(recordedBlobs, {type: 'video/webm'});
+        setTimeout(() => {
+            play(blob)
+            
+            return;
+            ws.send('filename:'+(idVideo++)+"test.webm");
+            ws.send(blob);
+            ws.send('end');
+            containerBlobs=[];
+        }, 10);
+    }
   }
 }
 
 function startRecording() {
+
   recordedBlobs = [];
   let options = {mimeType: 'video/webm;codecs=vp9'};
   if (!MediaRecorder.isTypeSupported(options.mimeType)) {
-    console.error(`${options.mimeType} is not Supported`);
+    //console.error(`${options.mimeType} is not Supported`);
     errorMsgElement.innerHTML = `${options.mimeType} is not Supported`;
     options = {mimeType: 'video/webm;codecs=vp8'};
     if (!MediaRecorder.isTypeSupported(options.mimeType)) {
-      console.error(`${options.mimeType} is not Supported`);
+      //console.error(`${options.mimeType} is not Supported`);
       errorMsgElement.innerHTML = `${options.mimeType} is not Supported`;
       options = {mimeType: 'video/webm'};
       if (!MediaRecorder.isTypeSupported(options.mimeType)) {
-        console.error(`${options.mimeType} is not Supported`);
+        //console.error(`${options.mimeType} is not Supported`);
         errorMsgElement.innerHTML = `${options.mimeType} is not Supported`;
         options = {mimeType: ''};
       }
@@ -93,12 +127,12 @@ function startRecording() {
   try {
     mediaRecorder = new MediaRecorder(window.stream, options);
   } catch (e) {
-    console.error('Exception while creating MediaRecorder:', e);
+    //console.error('Exception while creating MediaRecorder:', e);
     errorMsgElement.innerHTML = `Exception while creating MediaRecorder: ${JSON.stringify(e)}`;
     return;
   }
-
-  console.log('Created MediaRecorder', mediaRecorder, 'with options', options);
+    
+  //console.log('Created MediaRecorder', mediaRecorder, 'with options', options);
   recordButton.textContent = 'Stop Recording';
   playButton.disabled = true;
   downloadButton.disabled = true;
@@ -106,18 +140,17 @@ function startRecording() {
     console.log('Recorder stopped: ', event);
   };
   mediaRecorder.ondataavailable = handleDataAvailable;
-  mediaRecorder.start(10); // collect 10ms of data
-  console.log('MediaRecorder started', mediaRecorder);
+  mediaRecorder.start(0); // collect 10ms of data
+  //console.log('MediaRecorder started', mediaRecorder);
 }
 
 function stopRecording() {
   mediaRecorder.stop();
-  console.log('Recorded Blobs: ', recordedBlobs);
 }
 
 function handleSuccess(stream) {
   recordButton.disabled = false;
-  console.log('getUserMedia() got stream:', stream);
+  //console.log('getUserMedia() got stream:', stream);
   window.stream = stream;
   const gumVideo = document.querySelector('video#gum');
   gumVideo.srcObject = stream;
@@ -128,7 +161,7 @@ async function initCamera(constraints) {
     const stream = await navigator.mediaDevices.getUserMedia(constraints);
     handleSuccess(stream);
   } catch (e) {
-    console.error('navigator.getUserMedia error:', e);
+    //console.error('navigator.getUserMedia error:', e);
     errorMsgElement.innerHTML = `navigator.getUserMedia error:${e.toString()}`;
   }
 }
@@ -143,6 +176,73 @@ document.querySelector('button#start').addEventListener('click', async () => {
       width: 1280, height: 720
     }
   };
-  console.log('Using media constraints:', constraints);
   await initCamera(constraints);
 });
+
+
+document.querySelector('button#test').addEventListener('click', async () => {
+    startRecording();
+    setTimeout(1000,stopRecording);
+    const blob = new Blob(recordedBlobs, {type: 'video/webm'});
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+      a.style.display = 'none';
+      a.href = url;
+      //console.log(url)
+      return;
+      a.download = 'test.webm';
+      document.body.appendChild(a);
+      a.click();
+      setTimeout(() => {
+        ws.send('filename:'+file.name);
+        ws.send(blob);
+        ws.send('end');
+        document.body.removeChild(a);
+        window.URL.revokeObjectURL(url);
+      }, 100);
+});
+
+var ws;
+
+function connectChatServer() {
+    ws = new WebSocket(
+            "ws://localhost:8888/receive/fileserver");
+
+    ws.binaryType = "arraybuffer";
+    ws.onopen = function() {
+        alert("Connected.")
+    };
+
+    ws.onmessage = function(evt) {
+        alert(evt.msg);
+    };
+
+    ws.onclose = function() {
+        alert("Connection is closed...");
+    };
+    ws.onerror = function(e) {
+        alert(e.msg);
+    }
+
+}
+
+function sendFile() {
+    var file = document.getElementById('filename').files[0];
+    ws.send('filename:'+file.name);
+    var reader = new FileReader();
+    var rawData = new ArrayBuffer();            
+    //alert(file.name);
+
+    reader.loadend = function() {
+
+    }
+    reader.onload = function(e) {
+        rawData = e.target.result;
+        ws.send(rawData);
+        alert("the File has been transferred.")
+        ws.send('end');
+    }
+    reader.readAsArrayBuffer(file);
+}
+
+
