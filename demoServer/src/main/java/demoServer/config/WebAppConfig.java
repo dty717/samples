0e73d45fbfdb1608138428ce814fc47d33fe31f3
1875167603 <1875167603@qq.com>
2018-12-23 16:06:05 +0800

    WebAppConfig.java

--- demoServer/src/main/java/demoServer/config/WebAppConfig.java
+++ demoServer/src/main/java/demoServer/config/WebAppConfig.java
@@ -6,7 +6,6 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration  
-@EnableWebMvc  
 public class WebAppConfig extends WebMvcConfigurerAdapter {  
 
     @Override
