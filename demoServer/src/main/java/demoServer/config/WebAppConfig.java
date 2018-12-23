3ab53573e2ef878873feb7a6203dfbcea3e1e94c
1875167603 <1875167603@qq.com>
2018-12-23 16:17:03 +0800

    WebAppConfig.java

--- demoServer/src/main/java/demoServer/config/WebAppConfig.java
+++ demoServer/src/main/java/demoServer/config/WebAppConfig.java
@@ -10,7 +10,7 @@ public class WebAppConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addRedirectViewController("/", "index.html");
+        //registry.addRedirectViewController("/samples/", "index.html");
     }
 
 }
