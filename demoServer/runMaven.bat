de49233218a1916187192261dea0e1061fc8ba45
1875167603 <1875167603@qq.com>
2018-12-23 13:37:10 +0800

    by dty717

---
+++ demoServer/runMaven.bat
@@ -0,0 +1,2 @@
+cd C:\Users\xqy\Desktop\github\dty717\samples\demoServer
+mvn tomcat7:run
---
+++ demoServer/src/main/java/ServerJava/web/TestController.java
@@ -0,0 +1,33 @@
+package demoServer.web;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+
+@Controller
+public class TestController {
+    @RequestMapping(value = { "/go_test" })
+    @ResponseBody
+    public String getCalculateSimple() {
+        //model.addAttribute("user", getPrincipal());
+        try{
+            return "_test";
+            
+        }catch(Exception e){
+            return "null";
+        }
+    }
+    
+    
+}
