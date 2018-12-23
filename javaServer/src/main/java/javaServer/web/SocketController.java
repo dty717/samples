f7852087034e7ec3cd5e4235c3936a9a372fc181
1875167603 <1875167603@qq.com>
2018-12-23 12:00:10 +0800

    SocketController.java

--- javaServer/src/main/java/javaServer/web/SocketController.java
+++ javaServer/src/main/java/javaServer/web/SocketController.java
@@ -1,10 +1,17 @@
-package javaServer.web;
+package web;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
@@ -17,25 +24,41 @@ import org.springframework.web.util.HtmlUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 
-import javaServer.domain.Greeting;
-import javaServer.domain.HelloMessage;
+import service.CreateNewProject;
+import service.ShowTip;
+import service.JavaParser;
+import domain.Greeting;
+import domain.HelloMessage;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.charset.Charset;
 import java.util.*;
 
-@Controller
-public class HelloController {
-    
 
-    /*@Autowired
+@Controller
+public class SocketController {
+    
+    
+    @RequestMapping(value = { "work","workSpace" })
+    public String workHome() {
+        return "workspace.html";
+    }
+    
+    @RequestMapping(value = { "web","Web" })
+    public String webHome() {
+        return "webIDE.html";
+    }
+    
+    @Autowired
     private SimpMessagingTemplate template;
+    
     
     @RequestMapping("/update")
     @ResponseBody
@@ -43,12 +66,18 @@ public class HelloController {
         this.template.convertAndSend("/topic/greetings", new Greeting(content));
         return new Greeting("OK!");
     }
-
+    @RequestMapping("/test")
+    @ResponseBody
+    public Greeting greeting(@RequestParam("api_key")String api_key,@RequestParam("field1")String field) throws Exception {
+        //this.template.convertAndSend("/topic/greetings", new Greeting(content));
+        System.out.println(api_key+":"+field);
+        return new Greeting("OK!");
+    }
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
     public Greeting greeting(HelloMessage message) throws Exception {
         
         return new Greeting(HtmlUtils.htmlEscape(message.getName()));
-    }*/
+    }
     
 }
